492468159b980c496f1447f5c82e621f3e668871
Aditya <ladwa.aditya@gmail.com>
2016-09-06 16:00:21 +0530

    Added Daggae 2 dependency Injection lib

---
+++ app/src/main/java/com/example/aditya/notesapp/data/Note.java
@@ -0,0 +1,8 @@
+package com.example.aditya.notesapp.data;
+
+/**
+ * Created by Aditya on 06-Sep-16.
+ */
+
+public class Note {
+}
---
+++ app/src/main/java/com/example/aditya/notesapp/data/module/AppModule.java
@@ -0,0 +1,25 @@
+package com.example.aditya.notesapp.data.module;
+
+import android.app.Application;
+
+import javax.inject.Singleton;
+
+import dagger.Provides;
+
+/**
+ * Created by Aditya on 06-Sep-16.
+ */
+
+public class AppModule {
+    Application mApplication;
+
+    public AppModule(Application mApplication) {
+        this.mApplication = mApplication;
+    }
+
+    @Provides
+    @Singleton
+    Application provideApplication() {
+        return mApplication;
+    }
+}
