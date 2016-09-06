742330aa980a9049eb97c05a1e1150f4f2192411
Aditya <ladwa.aditya@gmail.com>
2016-09-06 17:05:42 +0530

    Created Note taking activity

---
+++ app/src/main/java/com/example/aditya/notesapp/data/AddNote.java
@@ -0,0 +1,25 @@
+package com.example.aditya.notesapp.data;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.example.aditya.notesapp.R;
+
+public class AddNote extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_note);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+}
