65216a239049d12d69c3cc543d642d989ef22fab
Aditya <ladwa.aditya@gmail.com>
2016-09-06 16:16:00 +0530

    Added Dagger net components

--- app/src/main/java/com/example/aditya/notesapp/NoteApp.java
+++ app/src/main/java/com/example/aditya/notesapp/NoteApp.java
@@ -2,6 +2,7 @@ package com.example.aditya.notesapp;
 
 import android.app.Application;
 
+import com.example.aditya.notesapp.data.component.DaggerNetComponent;
 import com.example.aditya.notesapp.data.component.NetComponent;
 import com.example.aditya.notesapp.data.module.AppModule;
 import com.example.aditya.notesapp.data.module.NetModule;
