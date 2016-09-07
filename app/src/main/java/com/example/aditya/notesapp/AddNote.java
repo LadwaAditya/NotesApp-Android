9f872195209da48ca73c0c4740be77783dfd1684
Aditya <ladwa.aditya@gmail.com>
2016-09-07 18:53:02 +0530

    Fect: Push notification is working

--- app/src/main/java/com/example/aditya/notesapp/AddNote.java
+++ app/src/main/java/com/example/aditya/notesapp/AddNote.java
@@ -60,7 +60,6 @@ public class AddNote extends AppCompatActivity implements View.OnClickListener {
                         @Override
                         public void onCompleted() {
                             Log.d(TAG, "Complete");
-                            startActivity(new Intent(AddNote.this, MainActivity.class));
                             finish();
                         }
 
--- app/src/main/java/com/example/aditya/notesapp/MainActivity.java
+++ app/src/main/java/com/example/aditya/notesapp/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.aditya.notesapp;
 
+import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -14,7 +20,14 @@ import android.widget.ListView;
 import com.example.aditya.notesapp.data.Count;
 import com.example.aditya.notesapp.data.Endpoint;
 import com.example.aditya.notesapp.data.Note;
+import com.ibm.mobilefirstplatform.clientsdk.android.core.api.BMSClient;
+import com.ibm.mobilefirstplatform.clientsdk.android.push.api.MFPPush;
+import com.ibm.mobilefirstplatform.clientsdk.android.push.api.MFPPushException;
+import com.ibm.mobilefirstplatform.clientsdk.android.push.api.MFPPushNotificationListener;
+import com.ibm.mobilefirstplatform.clientsdk.android.push.api.MFPPushResponseListener;
+import com.ibm.mobilefirstplatform.clientsdk.android.push.api.MFPSimplePushNotification;
 
+import java.net.MalformedURLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,10 +40,12 @@ import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemLongClickListener {
 
-    ListView mListView;
-    ArrayList<String> mList;
-    ArrayAdapter<String> mArrayAdapter;
+    private ListView mListView;
+    private ArrayList<String> mList;
+    private ArrayAdapter<String> mArrayAdapter;
     private List<Note> mNotes;
+    private static final int PERMISSION_GET_ACCOUNT = 100;
+    private MFPPush push;
 
     public static final String TAG = MainActivity.class.getSimpleName();
 
@@ -62,6 +77,29 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             }
         });
 
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.GET_ACCOUNTS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.GET_ACCOUNTS},
+                    PERMISSION_GET_ACCOUNT);
+
+        } else {
+            setUpBMSClient();
+        }
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        if (push != null) {
+            push.listen(notificationListener);
+        }
+
         mRetrofit.create(Endpoint.class).getNotes()
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
@@ -81,45 +119,118 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
                     @Override
                     public void onNext(List<Note> notes) {
+                        if (mNotes != null)
+                            mNotes.clear();
+                        if (mList != null)
+                            mList.clear();
 
                         for (int i = 0; i < notes.size(); i++) {
                             mNotes = notes;
                             mList.add(notes.get(i).getName());
-
                             Log.d(TAG, notes.get(i).getName() + notes.get(i).getId());
                         }
-
                     }
                 });
     }
+
+    private void setUpBMSClient() {
+
+        try {
+            BMSClient.getInstance().initialize(getApplicationContext(),
+                    "http://notesapp.mybluemix.net",
+                    "f54a78a7-8e23-450d-a6c6-b2553945acba",
+                    BMSClient.REGION_US_SOUTH);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            Log.d(TAG, "Auth error");
+        }
+
+        push = MFPPush.getInstance();
+        push.initialize(getApplicationContext(), "f54a78a7-8e23-450d-a6c6-b2553945acba");
+
+        push.registerDevice(new MFPPushResponseListener<String>() {
+            @Override
+            public void onSuccess(String deviceId) {
+                Log.d(TAG, "Registered for push");
+            }
+
+            @Override
+            public void onFailure(MFPPushException ex) {
+                Log.d(TAG, "Error registering device");
+            }
+        });
+        Log.d(TAG, "Auth called");
+    }
+
+    MFPPushNotificationListener notificationListener = new MFPPushNotificationListener() {
+        @Override
+        public void onReceive(final MFPSimplePushNotification message) {
+            Log.d(TAG, message.getAlert());
+        }
+    };
 
 
     @Override
     public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
         final int item = i;
-        mRetrofit.create(Endpoint.class).deleteNote(mNotes.get(i).getId())
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .unsubscribeOn(Schedulers.io())
-                .subscribe(new Observer<Count>() {
+
+        new AlertDialog.Builder(this)
+                .setTitle("Do you want to delete " + mNotes.get(i).getName())
+                .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
                     @Override
-                    public void onCompleted() {
-                        Log.d(TAG, "Completed");
-                        mList.remove(item);
-                        mArrayAdapter.notifyDataSetChanged();
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+
+                        mRetrofit.create(Endpoint.class).deleteNote(mNotes.get(item).getId())
+                                .subscribeOn(Schedulers.io())
+                                .observeOn(AndroidSchedulers.mainThread())
+                                .unsubscribeOn(Schedulers.io())
+                                .subscribe(new Observer<Count>() {
+                                    @Override
+                                    public void onCompleted() {
+                                        Log.d(TAG, "Completed");
+                                        mList.remove(item);
+                                        mArrayAdapter.notifyDataSetChanged();
+
+                                    }
+
+                                    @Override
+                                    public void onError(Throwable e) {
+                                        Log.d(TAG, e.toString());
+                                    }
+
+                                    @Override
+                                    public void onNext(Count count) {
+                                        Log.d(TAG, String.valueOf(count));
+                                    }
+                                });
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
 
                     }
+                }).create().show();
 
-                    @Override
-                    public void onError(Throwable e) {
-                        Log.d(TAG, e.toString());
-                    }
-
-                    @Override
-                    public void onNext(Count count) {
-                        Log.d(TAG, String.valueOf(count));
-                    }
-                });
         return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSION_GET_ACCOUNT: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    setUpBMSClient();
+
+
+                }
+
+            }
+        }
     }
 }
